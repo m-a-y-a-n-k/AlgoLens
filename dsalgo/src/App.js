d9314373aa4cb33b1a8a85a3878d409384943d22
Mayank Narula <mayanknarula96@gmail.com>
2019-10-04 14:09:12 +0530

    Added Segmented sieve of eratosthenes............................

--- dsalgo/src/App.js
+++ dsalgo/src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Arr from './ds/Array';
 import LL from './ds/LinkedList';
+import Sieve from './algo/Sieve';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <LL />
+      <Sieve />
     </div>
   );
 }
