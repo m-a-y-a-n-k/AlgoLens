25678e180417f965e1c64c19809e414067ce4206
MAYANK NARULA <mayanknarula96@gmail.com>
2021-07-20 20:59:50 +0530

    UI fix

--- src/base/App.js
+++ src/base/App.js
@@ -3,6 +3,7 @@ import SiteFooter from "./StickyFooter";
 import SiteHeader from "./StickyHeader";
 import Router from "../routing/base/Router";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
