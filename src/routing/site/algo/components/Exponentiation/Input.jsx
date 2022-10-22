0a3525688d4d9e28ad675357bb02f1aeff764fd3
Mayank N <mnarulasubs@gmail.com>
2022-10-22 16:37:37 +0530

    Exponentiation algo refactored with improved UX and Animation

---
+++ src/routing/site/algo/components/Exponentiation/Input.jsx
@@ -0,0 +1,120 @@
+import React, { useState } from "react"
+import { makeStyles, fade } from "@material-ui/core/styles"
+import Card from "@material-ui/core/Card"
+import CardActions from "@material-ui/core/CardActions"
+import Button from "@material-ui/core/Button"
+import TextField from "@material-ui/core/TextField"
+import { FormControl } from "@material-ui/core"
+
+//Card component styling
+const useStyles = makeStyles({
+  root: {
+    minWidth: 180,
+  },
+})
+
+//text input field styling
+const useStylesReddit = makeStyles((theme) => ({
+  root: {
+    border: "1px solid #e2e2e1",
+    overflow: "hidden",
+    borderRadius: 4,
+    backgroundColor: "#fcfcfb",
+    transition: theme.transitions.create(["border-color", "box-shadow"]),
+    "&:hover": {
+      backgroundColor: "#fff",
+    },
+    "&$focused": {
+      backgroundColor: "#fff",
+      boxShadow: `${fade(theme.palette.primary.main, 0.25)} 0 0 0 2px`,
+      borderColor: theme.palette.primary.main,
+    },
+  },
+  focused: {},
+}))
+function RedditTextField(props) {
+  const classes = useStylesReddit()
+
+  return (
+    <TextField InputProps={{ classes, disableUnderline: true }} {...props} />
+  )
+}
+
+const styleforform = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+    margin: theme.spacing(1),
+  },
+}))
+
+const stylefortext = makeStyles((theme) => ({
+  root: {
+    margin: theme.spacing(1),
+  },
+}))
+
+const styleforbutton = makeStyles((theme) => ({
+  root: {
+    margin: theme.spacing(1),
+  },
+}))
+
+const Input = (props) => {
+  const [base, setBase] = useState(null)
+  const [power, setPower] = useState(null)
+  const classes = useStyles()
+  const inputstyle = stylefortext()
+  const buttonstyle = styleforbutton()
+  const formstyle = styleforform()
+
+  return (
+    <Card className={classes.root} variant="outlined">
+      <h2 className="bg-success text-white p-2"> Exponentiation Of Number </h2>
+      <h4 className="text-primary p-2">Base and Power</h4>
+      <CardActions>
+        <FormControl className={`pb-3 pr-0 pl-2 pt-1 ${formstyle.root}`}>
+          <RedditTextField
+            label="Base"
+            className={inputstyle.root}
+            variant="filled"
+            id="reddit-input-base"
+            onChange={(event) => {
+              setBase(event.target.value)
+            }}
+            value={base ? base : ""}
+            disabled={props.disabled}
+          />
+          <RedditTextField
+            label="Power"
+            className={inputstyle.root}
+            variant="filled"
+            id="reddit-input-power"
+            onChange={(event) => {
+              setPower(event.target.value)
+            }}
+            value={power ? power : ""}
+            disabled={props.disabled}
+          />
+          <Button
+            className={buttonstyle.root}
+            disabled={props.disabled}
+            onClick={() => {
+              props.expo(parseFloat(base), parseInt(power))
+              setTimeout(() => {
+                setBase(null)
+                setPower(null)
+              }, 100)
+            }}
+            variant="contained"
+            color="primary"
+            size="small"
+          >
+            Submit
+          </Button>
+        </FormControl>
+      </CardActions>
+    </Card>
+  )
+}
+
+export default Input
--- src/routing/site/algo/components/Exponentiation/index.jsx
+++ src/routing/site/algo/components/Exponentiation/index.jsx
@@ -1,90 +1,21 @@
 import React from "react"
-import Element from "../../../../../common/components/Element"
-import {
-  Container,
-  Row,
-  Col,
-  Card,
-  CardBody,
-  CardHeader,
-  CardTitle,
-  Button,
-  InputGroup,
-  Input,
-  InputGroupAddon,
-  InputGroupText,
-} from "reactstrap"
+import Element from "common/components/Element"
+import Grid from "@material-ui/core/Grid"
+import Slide from "@material-ui/core/Slide"
 import { FaEquals, FaTimes } from "react-icons/fa"
-
-class Inp extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      base: null,
-      power: null,
-    }
-  }
-  render() {
-    return (
-      <Card style={{ border: "1px solid rgba(22,45,167,0.9)" }}>
-        <CardHeader>Exponentiation Of Number</CardHeader>
-        <CardBody className="text-center">
-          <CardTitle>Base and Power</CardTitle>
-          <InputGroup>
-            <InputGroupAddon addonType="prepend">
-              <InputGroupText>Base</InputGroupText>
-            </InputGroupAddon>
-            <Input
-              disabled={this.props.parent.state.disabled}
-              type="number"
-              placeholder="Base"
-              onChange={(event) => {
-                this.setState({ base: event.target.value })
-              }}
-              value={this.state.base ? this.state.base : ""}
-            />
-          </InputGroup>
-          <InputGroup>
-            <InputGroupAddon addonType="prepend">
-              <InputGroupText>Exponent</InputGroupText>
-            </InputGroupAddon>
-            <Input
-              disabled={this.props.parent.state.disabled}
-              type="number"
-              placeholder="Power"
-              onChange={(event) => {
-                this.setState({ power: event.target.value })
-              }}
-              value={this.state.power ? this.state.power : ""}
-            />
-          </InputGroup>
-          <br />
-          <Button
-            disabled={this.props.parent.state.disabled}
-            onClick={() => {
-              this.props.parent.expo(
-                parseFloat(this.state.base),
-                parseInt(this.state.power)
-              )
-              this.setState({ base: null, power: null })
-            }}
-          >
-            Submit
-          </Button>
-        </CardBody>
-      </Card>
-    )
-  }
-}
+import Input from "routing/site/algo/components/Exponentiation/Input"
 
 export default class Exponent extends React.Component {
-  state = {
-    result: [],
-    base: null,
-    power: null,
-    disabled: false,
-    ans: null,
+  constructor(props) {
+    super(props)
+    this.expo = this.expo.bind(this)
+    this.state = {
+      result: [],
+      base: null,
+      power: null,
+      disabled: false,
+      ans: null,
+    }
   }
 
   power(power) {
@@ -141,60 +72,77 @@ export default class Exponent extends React.Component {
 
   render() {
     return (
-      <Container>
-        <Row className="text-center">
-          <Col sm={6}>
-            <Inp parent={this} />
-          </Col>
-        </Row>
-        <Row className="text-center mt-4 mb-4">
-          {this.state.base && this.state.power && (
-            <React.Fragment>
+      <Grid container>
+        <Slide
+          direction="right"
+          in={!this.state.disabled}
+          mountOnEnter
+          unmountOnExit
+        >
+          <Grid item xs={12} sm={6} className="text-center m-4">
+            <Input disabled={this.state.disabled} expo={this.expo} />
+          </Grid>
+        </Slide>
+        <Slide
+          direction="left"
+          in={this.state.disabled || this.state.ans}
+          mountOnEnter
+          unmountOnExit
+        >
+          <Grid
+            item
+            xs={12}
+            sm={this.state.disabled ? 12 : 4}
+            className="text-center m-4"
+          >
+            {this.state.base && this.state.power && (
+              <React.Fragment>
+                <Element
+                  highlight={true}
+                  data={{ value: `${this.state.base} ^ ${this.state.power}` }}
+                  type="Array"
+                />
+                <FaEquals style={{ margin: "auto 5px" }} />
+              </React.Fragment>
+            )}
+            {this.state.result.length >= 1 &&
+              this.state.disabled &&
+              this.state.result.map((value, index, result) => {
+                if (index > 0) {
+                  return (
+                    <React.Fragment key={`res_mul_val_${index}`}>
+                      <FaTimes style={{ margin: "auto 5px" }} />
+                      <Element data={{ value }} type="Array" />
+                    </React.Fragment>
+                  )
+                } else {
+                  let highlight
+                  if (result.length === 1) {
+                    highlight = true
+                  } else {
+                    highlight = false
+                  }
+                  return (
+                    <Element
+                      key="first_val"
+                      highlight={highlight}
+                      data={{ value }}
+                      type="Array"
+                    />
+                  )
+                }
+              })}
+            {!this.state.disabled && this.state.ans && (
               <Element
+                key="result"
                 highlight={true}
-                data={{ value: `${this.state.base} ^ ${this.state.power}` }}
+                data={{ value: this.state.ans }}
                 type="Array"
               />
-              <FaEquals style={{ margin: "auto 5px" }} />
-            </React.Fragment>
-          )}
-          {this.state.result.length >= 1 &&
-            this.state.disabled &&
-            this.state.result.map((value, index, result) => {
-              if (index > 0) {
-                return (
-                  <React.Fragment key={`res_mul_val_${index}`}>
-                    <FaTimes style={{ margin: "auto 5px" }} />
-                    <Element data={{ value }} type="Array" />
-                  </React.Fragment>
-                )
-              } else {
-                let highlight
-                if (result.length === 1) {
-                  highlight = true
-                } else {
-                  highlight = false
-                }
-                return (
-                  <Element
-                    key="first_val"
-                    highlight={highlight}
-                    data={{ value }}
-                    type="Array"
-                  />
-                )
-              }
-            })}
-          {!this.state.disabled && this.state.ans && (
-            <Element
-              key="result"
-              highlight={true}
-              data={{ value: this.state.ans }}
-              type="Array"
-            />
-          )}
-        </Row>
-      </Container>
+            )}
+          </Grid>
+        </Slide>
+      </Grid>
     )
   }
 }
