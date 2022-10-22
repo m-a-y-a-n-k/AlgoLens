63f0c2435e65f07050391e7e5854dc9178088a7f
Mayank N <mnarulasubs@gmail.com>
2022-10-22 18:10:03 +0530

    Refactored Factorial algo with improved UX

--- src/routing/site/algo/components/Factorial/index.jsx
+++ src/routing/site/algo/components/Factorial/index.jsx
@@ -1,82 +1,28 @@
 import React from "react"
-import Element from "../../../../../common/components/Element"
+import Element from "common/components/Element"
 import { FaEquals, FaTimes } from "react-icons/fa"
-import {
-  Button,
-  Card,
-  CardContent,
-  CardHeader,
-  Grid,
-  TextField,
-} from "@material-ui/core"
+import { Grid } from "@material-ui/core"
 import { Alert } from "reactstrap"
-
-class Inp extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      input: null,
-    }
-  }
-  render() {
-    return (
-      <Card>
-        <CardHeader
-          title={"Factorial Of Number"}
-          titleTypographyProps={{
-            variant: "h5",
-            color: "primary",
-          }}
-          subheader="Finds the product 1 x 2 x 3 x .... upto a number N"
-          subheaderTypographyProps={{
-            variant: "subtitle1",
-            color: "secondary",
-          }}
-        ></CardHeader>
-        <CardContent
-          style={{ display: "flex", flexDirection: "column" }}
-          className="text-center"
-        >
-          <TextField
-            disabled={this.props.parent.state.disabled}
-            type="number"
-            label="Factorial Of Number"
-            onChange={(event) => {
-              this.setState({ input: event.target.value })
-            }}
-            value={this.state.input ?? ""}
-          />
-          <br />
-          <Button
-            style={{
-              marginTop: 12,
-              backgroundColor: "#403d4a",
-              color: "white",
-            }}
-            type="submit"
-            disabled={this.props.parent.state.disabled}
-            onClick={() => {
-              this.props.parent.fact(parseInt(this.state.input))
-              this.setState({ input: null })
-              this.props.parent.setState({ result: null })
-            }}
-          >
-            Submit
-          </Button>
-        </CardContent>
-      </Card>
-    )
-  }
-}
+import Input from "routing/site/algo/components/Factorial/Input"
 
 export default class Factorial extends React.Component {
-  state = {
-    input: null,
-    result: null,
-    processed: null,
-    rest: null,
-    disabled: false,
+  constructor(props) {
+    super(props)
+    this.state = {
+      input: null,
+      result: null,
+      processed: null,
+      rest: null,
+      disabled: false,
+    }
+    this.setResult = this.setResult.bind(this)
+    this.fact = this.fact.bind(this)
+  }
+
+  setResult(result) {
+    this.setState({
+      result,
+    })
   }
 
   fact(input) {
@@ -140,7 +86,7 @@ export default class Factorial extends React.Component {
     return (
       <Grid container>
         {this.state.alert && (
-          <Grid item sm={12}>
+          <Grid item xs={12}>
             <Alert
               color={this.state.alert.type}
               isOpen={!!this.state.alert.text}
@@ -153,30 +99,34 @@ export default class Factorial extends React.Component {
           </Grid>
         )}
         <Grid container className="text-center">
-          <Grid item sm={12}>
-            <Inp parent={this} />
+          <Grid item xs={12}>
+            <Input
+              fact={this.fact}
+              setResult={this.setResult}
+              disabled={this.state.disabled}
+            />
           </Grid>
         </Grid>
 
         {parseInt(this.state.input) >= 0 && (
           <Grid container className="text-center mt-4 mb-4">
-            <Grid item sm={2}>
+            <Grid item xs={2}>
               <Element
                 highlight={true}
                 data={{ value: `${this.state.input}!` }}
                 type="Array"
               />
             </Grid>
-            <Grid item sm={2}>
+            <Grid item xs={2}>
               <FaEquals style={{ margin: "auto 5px" }} />
             </Grid>
-            <Grid item sm={3}>
+            <Grid item xs={3}>
               <Element data={{ value: this.state.processed }} type="Array" />
             </Grid>
-            <Grid item sm={2}>
+            <Grid item xs={2}>
               <FaTimes style={{ margin: "auto 5px" }} />
             </Grid>
-            <Grid item sm={3}>
+            <Grid item xs={3}>
               <Element data={{ value: `${this.state.rest}!` }} type="Array" />
             </Grid>
           </Grid>
