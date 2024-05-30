e5b1841cafad71c24a40432da9e68b234a30dbdb
mayanknarula96 <mnarulasubs@gmail.com>
2024-05-30 17:05:04 +0530

    Convert Exponentiation to FC

--- src/routing/site/algo/components/Exponentiation/index.jsx
+++ src/routing/site/algo/components/Exponentiation/index.jsx
@@ -1,148 +1,122 @@
-import React from "react"
-import Element from "common/components/Element"
+import React, { useState, useCallback, useEffect, lazy } from "react"
 import Grid from "@material-ui/core/Grid"
 import Slide from "@material-ui/core/Slide"
 import { FaEquals, FaTimes } from "react-icons/fa"
 import Input from "routing/site/algo/components/Exponentiation/Input"
 
-export default class Exponent extends React.Component {
-  constructor(props) {
-    super(props)
-    this.expo = this.expo.bind(this)
-    this.state = {
-      result: [],
-      base: null,
-      power: null,
-      disabled: false,
-      ans: null,
-    }
-  }
+const Element = lazy(() => import("common/components/Element"))
 
-  power(power) {
-    if (power > 0)
-      this.setState(
-        (prevState) => {
-          let ans,
-            result = [...prevState.result],
-            disabled,
-            p
-          for (p = 0; p < power - 1; p += 2) {
-            result[p / 2] = result[p] * result[p + 1]
-          }
-          if (power % 2 === 1) {
-            result[p / 2] = result[power - 1]
-          }
-          if (result.length === 1) {
-            ans = result[0]
-            result.length = 0
-            disabled = false
-          } else {
-            disabled = true
-            result.length = Math.ceil(power / 2)
-          }
-          return { result, disabled, ans }
-        },
-        () => {
-          setTimeout(() => {
-            this.power(!this.state.ans ? Math.ceil(power / 2) : 0)
-          }, 1200)
-        }
-      )
-  }
+const Exponent = () => {
+  const [state, setState] = useState({
+    result: [],
+    base: null,
+    power: null,
+    disabled: false,
+    ans: null,
+  })
 
-  expo(base, power) {
-    let result = [...this.state.result]
-    for (let p = 0; p < power; p++) {
-      result.push(base)
+  const calculatePower = useCallback((currentPower) => {
+    setState((prevState) => {
+      const newResult = []
+      for (let p = 0; p < currentPower - 1; p += 2) {
+        newResult.push(prevState.result[p] * prevState.result[p + 1])
+      }
+      if (currentPower % 2 === 1) {
+        newResult.push(prevState.result[currentPower - 1])
+      }
+
+      const isFinal = newResult.length === 1
+      return {
+        ...prevState,
+        result: newResult,
+        disabled: !isFinal,
+        ans: isFinal ? newResult[0] : prevState.ans,
+      }
+    })
+  }, [])
+
+  useEffect(() => {
+    if (state.disabled && state.result.length > 0) {
+      const timer = setTimeout(() => {
+        calculatePower(Math.ceil(state.result.length))
+      }, 1200)
+      return () => clearTimeout(timer)
     }
-    this.setState(
-      {
-        result,
+  }, [state.disabled, state.result, calculatePower])
+
+  const expo = useCallback(
+    (base, power) => {
+      if (power === 0) {
+        setState({ result: [], base, power, disabled: false, ans: 1 })
+        return
+      }
+
+      const initialResult = Array(power).fill(base)
+      setState({
+        result: initialResult,
         base,
         power,
         disabled: true,
-      },
-      () => {
-        setTimeout(() => {
-          this.power(power)
-        }, 500)
-      }
-    )
-  }
+        ans: null,
+      })
+      setTimeout(() => {
+        calculatePower(power)
+      }, 500)
+    },
+    [calculatePower]
+  )
 
-  render() {
-    return (
-      <Grid container>
-        <Slide
-          direction="right"
-          in={!this.state.disabled}
-          mountOnEnter
-          unmountOnExit
-        >
-          <Grid item xs={12} sm={6} className="text-center m-4">
-            <Input disabled={this.state.disabled} expo={this.expo} />
-          </Grid>
-        </Slide>
-        <Slide
-          direction="left"
-          in={this.state.disabled || this.state.ans}
-          mountOnEnter
-          unmountOnExit
-        >
-          <Grid
-            item
-            xs={12}
-            sm={this.state.disabled ? 12 : 4}
-            className="text-center m-4"
-          >
-            {this.state.base && this.state.power && (
-              <React.Fragment>
-                <Element
-                  highlight={true}
-                  data={{ value: `${this.state.base} ^ ${this.state.power}` }}
-                  type="array"
-                />
-                <FaEquals style={{ margin: "auto 5px" }} />
-              </React.Fragment>
-            )}
-            {this.state.result.length >= 1 &&
-              this.state.disabled &&
-              this.state.result.map((value, index, result) => {
-                if (index > 0) {
-                  return (
-                    <React.Fragment key={`res_mul_val_${index}`}>
-                      <FaTimes style={{ margin: "auto 5px" }} />
-                      <Element data={{ value }} type="array" />
-                    </React.Fragment>
-                  )
-                } else {
-                  let highlight
-                  if (result.length === 1) {
-                    highlight = true
-                  } else {
-                    highlight = false
-                  }
-                  return (
-                    <Element
-                      key="first_val"
-                      highlight={highlight}
-                      data={{ value }}
-                      type="array"
-                    />
-                  )
-                }
-              })}
-            {!this.state.disabled && this.state.ans && (
+  const { result, base, power, disabled, ans } = state
+
+  return (
+    <Grid container>
+      <Slide direction="right" in={!disabled} mountOnEnter unmountOnExit>
+        <Grid item xs={12} sm={6} className="text-center m-4">
+          <Input disabled={disabled} expo={expo} />
+        </Grid>
+      </Slide>
+      <Slide
+        direction="left"
+        in={disabled || ans !== null}
+        mountOnEnter
+        unmountOnExit
+      >
+        <Grid item xs={12} sm={disabled ? 12 : 4} className="text-center m-4">
+          {base !== null && power !== null && (
+            <>
               <Element
-                key="result"
                 highlight={true}
-                data={{ value: this.state.ans }}
+                data={{ value: `${base} ^ ${power}` }}
                 type="array"
               />
-            )}
-          </Grid>
-        </Slide>
-      </Grid>
-    )
-  }
+              <FaEquals style={{ margin: "auto 5px" }} />
+            </>
+          )}
+          {result.length >= 1 &&
+            disabled &&
+            result.map((value, index) => (
+              <React.Fragment key={`res_mul_val_${index}`}>
+                {index > 0 && <FaTimes style={{ margin: "auto 5px" }} />}
+                <Element
+                  key={`val_${index}`}
+                  highlight={result.length === 1}
+                  data={{ value }}
+                  type="array"
+                />
+              </React.Fragment>
+            ))}
+          {!disabled && ans !== null && (
+            <Element
+              key="result"
+              highlight={true}
+              data={{ value: ans }}
+              type="array"
+            />
+          )}
+        </Grid>
+      </Slide>
+    </Grid>
+  )
 }
+
+export default Exponent
