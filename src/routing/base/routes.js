7493692ba597a9e74507ce35d72215d8ae95d1f7
mayanknarula96 <mnarulasubs@gmail.com>
2024-05-30 16:36:24 +0530

    Update route slugs

--- src/routing/base/routes.js
+++ src/routing/base/routes.js
@@ -1,46 +1,109 @@
 const pages = [
   {
-    algo: [
-      "BinarySearch",
-      "ClosestPair",
-      "Exponentiation",
-      "Factorial",
-      "Factorize",
-      "Fibonacci",
-      "FloodFill",
-      "KnuthShuffle",
-      "NQueens",
-      "Palindrome",
-      "Peak",
-      "Sieve",
+    topic: "algo",
+    label: "Algorithms",
+    pages: [
+      {
+        topic: "BinarySearch",
+        label: "Binary Search",
+      },
+      {
+        topic: "ClosestPair",
+        label: "Closest Pair",
+      },
+      {
+        topic: "Exponentiation",
+        label: "Exponentiation",
+      },
+      {
+        topic: "Factorial",
+        label: "Factorial",
+      },
+      {
+        topic: "Factorize",
+        label: "Factorize",
+      },
+      {
+        topic: "Fibonacci",
+        label: "Fibonacci",
+      },
+      {
+        topic: "FloodFill",
+        label: "Flood Fill",
+      },
+      {
+        topic: "KnuthShuffle",
+        label: "Knuth Shuffle",
+      },
+      {
+        topic: "NQueens",
+        label: "NQueens",
+      },
+      {
+        topic: "Palindrome",
+        label: "Palindrome",
+      },
+      {
+        topic: "Peak",
+        label: "Peak",
+      },
+      {
+        topic: "Sieve",
+        label: "Sieve",
+      },
     ],
   },
   {
-    ds: ["Array", "LinkedList", "Lookup", "Queues", "Sets", "Stacks"],
+    topic: "ds",
+    label: "Data Structures",
+    pages: [
+      {
+        topic: "Array",
+        label: "Array",
+      },
+      {
+        topic: "LinkedList",
+        label: "Linked List",
+      },
+      {
+        topic: "Lookup",
+        label: "Lookup",
+      },
+      {
+        topic: "Queues",
+        label: "Queues",
+      },
+      {
+        topic: "Sets",
+        label: "Sets",
+      },
+      {
+        topic: "Stacks",
+        label: "Stacks",
+      },
+    ],
   },
 ]
 
-const dynamicRoute = (category, subject) => {
-  return `${category}/${subject}`
+const dynamicRoute = (categoryTopic, subjectTopic) => {
+  return `${categoryTopic}/${subjectTopic}`
 }
 
-const dynamicPath = (category, subject) => {
-  return `site/${category}/components/${subject}`
+const dynamicPath = (categoryTopic, subjectTopic) => {
+  return `site/${categoryTopic}/components/${subjectTopic}`
 }
 
 const siteSugg = []
 
-pages.forEach((page) => {
-  Object.keys(page).forEach((category) => {
-    page[category].forEach((subject) => {
-      const path = `${dynamicPath(category, subject)}`
-      const route = `/${dynamicRoute(category, subject)}`
-      const title = `${category.toUpperCase()} : ${subject}`
-      siteSugg.push({
-        route,
-        title,
-        path,
-      })
+pages.forEach((category) => {
+  category.pages.forEach((subject) => {
+    const path = `${dynamicPath(category.topic, subject.topic)}`
+    const route = `/${dynamicRoute(category.topic, subject.topic)}`
+    const title = `${category.label} : ${subject.label}`
+    siteSugg.push({
+      route,
+      title,
+      path,
     })
   })
 })
