b4e99048126603ef2524ca0ddb9487dc5762c6a8
mayanknarula96 <mnarulasubs@gmail.com>
2024-06-08 17:27:02 +0530

    Fixed Side Drawer open close toggle bug

--- src/base/FixedSideDrawer.jsx
+++ src/base/FixedSideDrawer.jsx
@@ -24,7 +24,7 @@ const SideDrawer = () => {
   })
 
   return (
-    <div className="header">
+    <div className="header" ref={sideDrawerRef}>
       <div
         className={`hamburger ${isOpen ? "open" : ""}`}
         onClick={toggleDrawer}
@@ -33,10 +33,7 @@ const SideDrawer = () => {
         <div className="bar"></div>
         <div className="bar"></div>
       </div>
-      <div
-        ref={sideDrawerRef}
-        className={`side-drawer ${isOpen ? "show" : "hide"}`}
-      >
+      <div className={`side-drawer ${isOpen ? "show" : "hide"}`}>
         {pages.map((section) => (
           <div key={section.topic}>
             <h3 onClick={() => toggleSection(section.topic)}>
