299a28da8351aea8e9569ef5c9a8ff6f50e6fef2
mayanknarula96 <mnarulasubs@gmail.com>
2024-06-09 16:40:21 +0530

    Updates

--- static/js/7233.06b56792.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkapp=self.webpackChunkapp||[]).push([[7233],{17233:(e,a,s)=>{s.r(a),s.d(a,{default:()=>p});var o=s(87462),t=s(45987),n=s(72791),r=(s(52007),s(28182)),c=s(38317),l=n.forwardRef((function(e,a){var s=e.classes,c=e.className,l=e.component,p=void 0===l?"div":l,u=(0,t.Z)(e,["classes","className","component"]);return n.createElement(p,(0,o.Z)({ref:a,className:(0,r.Z)(s.root,c)},u))}));const p=(0,c.Z)({root:{width:"100%",overflowX:"auto"}},{name:"MuiTableContainer"})(l)}}]);
-//# sourceMappingURL=7233.06b56792.chunk.js.map
---
+++ static/js/7233.172e2b41.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkapp=self.webpackChunkapp||[]).push([[7233],{17233:(e,a,s)=>{s.r(a),s.d(a,{default:()=>u});var o=s(87462),t=s(45987),n=s(72791),r=(s(52007),s(28182)),c=s(38317),l=n.forwardRef((function(e,a){var s=e.classes,c=e.className,l=e.component,u=void 0===l?"div":l,f=(0,t.Z)(e,["classes","className","component"]);return n.createElement(u,(0,o.Z)({ref:a,className:(0,r.default)(s.root,c)},f))}));const u=(0,c.Z)({root:{width:"100%",overflowX:"auto"}},{name:"MuiTableContainer"})(l)}}]);
+//# sourceMappingURL=7233.172e2b41.chunk.js.map
